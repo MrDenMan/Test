3cb0c2f51a3cd51bdeb8df0986357209be5d5202
Denis <den2014is@yandex.ru>
2022-02-12 22:47:21 +0300

    1b - 2 минимальных значения

--- ex1.rb
+++ ex1.rb
@@ -2,6 +2,11 @@ def max_2(arr)
   arr.sort.reverse[0..1]
 end
   
+def min_2(arr)
+  arr.sort[0..1]
+end
+  
 array = [1, 2, 12, 34, 35, 6, 0, 34, 122, 124, 789, 999, 33, 54, 763, 893 ] 
   
 puts max_2(array)
+puts min_2(array)
