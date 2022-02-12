42c257e917d973c924efa372956be3cbd9758534
Denis <den2014is@yandex.ru>
2022-02-12 23:34:12 +0300

    4b,c - консольная программа

--- ex4.rb
+++ ex4.rb
@@ -1,4 +1,13 @@
 def to_Fahrenheit(degree)
   (degree * 9/5.to_f) + 32
 end
-puts to_Fahrenheit(23)
+#puts to_Fahrenheit(23)
+begin
+  print "Введите градусы по Цельсию : "
+  user_degree = Float(gets.chomp)
+  puts "Градусы по Фаренгейту : #{to_Fahrenheit(user_degree.to_f)}"
+  rescue => exception
+  puts "Введены неправильные данные! (#{exception.class})"
+  retry
+end
+      
